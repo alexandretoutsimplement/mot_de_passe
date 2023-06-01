e1159ecb0907a41715c07fd2ade17652b85976c5
alexandretoutsimplement <115820918+alexandretoutsimplement@users.noreply.github.com>
2023-06-01 23:32:29 +0200

    Update mot de passe.py
    en anglais c'est mieux

--- mot de passe.py
+++ mot de passe.py
@@ -39,32 +39,32 @@ def generate_password_and_save():
         with open(filename, 'a', newline='') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow([web, user, hashed_password])
-        messagebox.showinfo("Enregistrement terminé", f"Le mot de passe a été enregistré dans {filename}")
+        messagebox.showinfo("It's save")
 
 # Création de la fenêtre principale
 window = tkinter.Tk()
-window.title("Générateur de mot de passe")
+window.title("password generator")
 
-web_label= tkinter.Label(window, text="Entrer le nom du site")
+web_label= tkinter.Label(window, text="Enter URL")
 web_label.pack()
 web_entry = tkinter.Entry(window)
 web_entry.pack()
 
-user_label= tkinter.Label(window, text="Entrer le nom d'utilisateur")
+user_label= tkinter.Label(window, text="Enter user name ")
 user_label.pack()
 user_entry=tkinter.Entry(window)
 user_entry.pack()
 # Étiquettes et champs de saisie pour les informations
-length_label = tkinter.Label(window, text="Taille du mot de passe :")
+length_label = tkinter.Label(window, text="Length password")
 length_label.pack()
 length_entry = tkinter.Entry(window)
 length_entry.pack()
 
 special_chars_var = tkinter.BooleanVar()
-special_chars_checkbutton = tkinter.Checkbutton(window, text="Inclure des caractères spéciaux", variable=special_chars_var)
+special_chars_checkbutton = tkinter.Checkbutton(window, text="Include special chars", variable=special_chars_var)
 special_chars_checkbutton.pack()
 
-generate_button = tkinter.Button(window, text="Générer et Enregistrer", command=generate_password_and_save)
+generate_button = tkinter.Button(window, text="Create and save", command=generate_password_and_save)
 generate_button.pack()
 
 # Sortie du mot de passe généré
